9a32b8d471bd7049578f9b678781577ec13b4113
hskeays <hskeays@gmail.com>
2024-02-20 19:29:14 -0500

    Add sorting

--- src/Main.java
+++ src/Main.java
@@ -2,14 +2,18 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        int[] array = {1, 2, 3, 4, 5};
-        int value = 3;
+        int[] array = {5, 3, 2, 4, 0, 7, 9, 6, 1, 8};
+        Arrays.sort(array);
+        int value = 1;
         int answer = binarySearch(array, value);
 
+        System.out.print("\nIf the provided array was not sorted, ");
+        System.out.println("an element's index may have changed after sorting.\n");
+
         if (binarySearch(array, value) == -1) {
-            System.out.println("Value does not exist in the array");
+            System.out.println("The value does not exist in the array.");
         } else {
-            System.out.println("The index of value " + value + ": " + answer);
+            System.out.println("The index with value " + value + ": " + answer);
         }
     }
 
